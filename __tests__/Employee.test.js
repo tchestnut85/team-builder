f1da23d283d05cf2851e7d69b0f07c6f222b680f
Tom Chestnut <tchestnut85@gmail.com>
2020-10-10 11:48:37 -0400

    added html function

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,11 +3,10 @@ const Employee = require('../lib/Employee');
 test('creates an employee object', () => {
     const employee = new Employee('Tom');
 
-    expect(employee.name).toBe('Tom');
+    expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     // expect(employee.id).toHaveLength(4);
-    expect(employee.email).toBe(
-        expect.stringContaining('.com')
+    expect(employee.email).toBe(expect.stringContaining('.com')
     );
 
 });
--- index.js
+++ index.js
@@ -8,6 +8,45 @@ const createTeam = () => {
     new Employee().getName();
 };
 
+
+const generateHtml = () => {
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Builder</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
+        <main>
+            
+        </main>
+    </body>
+    </html>
+    `;
+};
+
+const newEmployee = () => {
+    inquirer
+        .prompt({
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Do you want to add another employee?',
+            validate: addInput => {
+                if (addInput) {
+                    getName();
+                } else {
+                    generateHtml();
+                }
+            }
+        });
+};
+
 createTeam();
 
 module.exports = Index;
