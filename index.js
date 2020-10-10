1e5d9e5a8ae78c3c95563d37254a055d64c0cf27
Tom Chestnut <tchestnut85@gmail.com>
2020-10-09 21:38:52 -0400

    setup files

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const Employee = require('./Employee');
+const Manager = require('./Manager');
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
+
